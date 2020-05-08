222b572a1fbfcbbd121717c23895b00b68ca822a
rhinnawi <rhinnawi95@gmail.com>
2020-05-08 00:21:10 -0400

    Set up html templates, Bootstrap, tested outputting MySQL query results

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
-#from flask_bootstrap import Bootstrap
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 from flask_mysqldb import MySQL
 import yaml
 
 app = Flask(__name__)
+Bootstrap(app)
 
 # Configure db using settings in db.yaml file
 db = yaml.load(open('db.yaml'))
@@ -15,14 +16,17 @@ app.config['MYSQL_DB'] = db['mysql_db']
 # Instantiate MySQL module
 mysql = MySQL(app) 
 
-
+# Home page
 @app.route('/')
-def index():
+def home():
     # Set up MySQL cursor
     crs = mysql.connection.cursor()
+
+    # Output tables to console. Will convert this to html later
     crs.execute("SHOW TABLES;")
-    mysql.connection.commit()
-    return 'Hallo, Welt!'
+    print([column[0] for column in crs.fetchall()])
+    #mysql.connection.commit() # Commit changes to db
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
